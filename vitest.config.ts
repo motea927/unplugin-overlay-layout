b27ad7bddbc59f048a55a97d0c53b99ab288aff5
李學澤Morty Li <motea927@yahoo.com.tw>
2024-01-15 15:30:06 +0800

    test: add IconDraggable test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/popup/src/'
+    }
+  },
   plugins: [vue()],
   test: {
-    // ... Specify options here.
     globals: true,
     environment: 'jsdom'
   }
